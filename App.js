758bcd80814f62d844923fb8950fbeb803e960df
Abdulhanan <buttabdulhannan708@gmail.com>
2022-10-09 20:27:10 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./Components/Header"
+import Meme from "./Components/Meme"
+export default function App() {
+    return(
+        <div>
+        <Header/>
+        <Meme/>
+        </div>
+    )
+}
---
+++ Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function Header(){
+    return(
+        <header className="Header">
+        <img src="images/troll-face.png" className="Header--img"/>
+        <h2 className="Header--heading">Meme Generator</h2>
+        </header>
+    )
+}
